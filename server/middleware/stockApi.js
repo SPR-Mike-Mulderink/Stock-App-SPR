e3385346353cbeddea31a77cb4f2b40a1ffd8315
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-10-05 01:34:04 -0500

    Refactored Fetch to a Promise.all

--- server/middleware/stockApi.js
+++ server/middleware/stockApi.js
@@ -2,26 +2,13 @@ const fetch = require('node-fetch');
 const API_KEY = `PXZD0QBQQZTN4V5U`;
 
 module.exports = (req, res, next) => {
-  const stocksArr = req.body.stocksArr;
-  const stocksValueArr = [];
-
-  async function fetchAsync() {
-    for (let i = 0; i < stocksArr.length; i += 1) {
-      const stock = stocksArr[i];
-      const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${API_KEY}`);
-      const data = await response.json();
-      const globalQuote = data['Global Quote'] ? data['Global Quote']['05. price'] : data;
-      stocksValueArr.push({ stock, globalQuote });
-    };
-  };
-
-  if (stocksArr == undefined || stocksArr.length <= 0) {
-    next();
-  } else {
-    fetchAsync()
-      .then(() => {
-        res.locals.stocksValueArr = stocksValueArr;
-        next()
-      });
-  };
+  Promise.all(req.body.stocksArr.map(stock =>
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => { return { value: json['Global Quote'] ? json['Global Quote']['05. price'] : `Failed to Fetch`, stock: stock } })
+  ))
+    .then(arr => {
+      res.locals.stocksValueArr = arr;
+      return next();
+    });
 };
